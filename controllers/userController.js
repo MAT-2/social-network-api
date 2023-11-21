de3526cd145dca1d34b0695a7763b12972ee557f
MAT-2 <mpcasey30@gmail.com>
2023-11-21 17:50:24 -0600

    Added logic for when a User is deleted, as well as their Thoughts being deleted if the user was deleted.

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,4 +45,36 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  //Deleting a user from database by ID.
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndRemove({
+        _id: req.params.userId,
+      });
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "There is no user that exists." });
+      }
+      //Logic for when a user's associated thoughts when deleted.
+      const thought = await Thought.findOneAndUpdate(
+        { user: req.params.userId },
+        { $pull: { user: req.params.userId } },
+        { new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({
+          message: "User was deleted, but there was no thoughts found",
+        });
+      }
+
+      res.json({ message: "A user was successfully deleted!" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
