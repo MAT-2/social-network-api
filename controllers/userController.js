e12c817dcb43d0992363b26d87bcf405b1e35333
MAT-2 <mpcasey30@gmail.com>
2023-11-21 17:44:53 -0600

    Added details in userController.js. Added functinal logic when doing GET requests for getting all users or a single user. Also added logic that will be used when creating a new user via a POST request.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,48 @@
 const { ObjectId } = require("mongoose").Types;
 const { User, Thought } = require("../models");
+
+//Exporting data from User model to get all users.
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      const userObj = {
+        users,
+      };
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  //Exporting data to get a single user.
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({
+        _id: req.params.userId,
+      }).select("-__v"); //that gives version number of document whenever something is updated and increments it.
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No user with that ID. Try again!" });
+      }
+      res.json({
+        user,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  //Creating a new user, using User Model as reference.
+  async userCreation(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
