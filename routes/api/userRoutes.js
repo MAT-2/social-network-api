3e573e239ec4d510a24a7a3d93ea2a5c05339b20
MAT-2 <mpcasey30@gmail.com>
2023-11-22 18:15:26 -0600

    Updated userRoutes.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,16 @@
 const router = require("express").Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  userCreation,
+  deleteUser,
+} = require("../../controllers/userController.js");
+
+// /api/user
+router.route("/").get(getUsers).post(userCreation);
+
+// /api/user/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
+
+module.exports = router;
